99f41c95d95daa0cba9bfb0e90d78f82426d4d52
Vincent Letarouilly <vincent.letarouilly@protonmail.com>
2024-02-01 14:45:00 +0100

    Write state of all buttons & knobs

--- src/utils.rs
+++ src/utils.rs
@@ -11,3 +11,21 @@ pub fn get_serial_number<T: UsbContext>(dev: &Device<T>) -> String {
         .read_serial_number_string(languages[0], &descriptor, timeout)
         .unwrap_or_default()
 }
+
+pub fn hex2bool(hex: u8, bin: &mut [bool; 8]) {
+    for i in 0..8 {
+        bin[i] = ((hex >> i) & 1) != 0;
+    }
+}
+
+pub fn hex2bin(hex: u8, bin: &mut [u8; 8]) {
+    for i in 0..8 {
+        bin[i] = (hex >> i) & 1;
+    }
+}
+
+pub fn knob_to_midi(knob: [char; 2]) -> u8 {
+    let combined_value = ((knob[0] as u16) << 8) | knob[1] as u16;
+    let midi_value = (combined_value as f32 / 0xFFF as f32 * 127.0).round() as u8;
+    return midi_value;
+}
