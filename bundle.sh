6aad81f75680b50a8c55f837c9086030e6ac4038
Vincent Letarouilly <vincent.letarouilly@protonmail.com>
2024-05-11 13:28:54 +0200

    Build target in their own steps

--- bundle.sh
+++ bundle.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Install libusb
-brew install libusb
-# Add x86_64 target for older macOS versions
-rustup target add x86_64-apple-darwin
+BUILD_TARGET=$1
+
+# Add build targets
+rustup target add $BUILD_TARGET
 # Bundle application
 cargo install cargo-bundle
-cargo bundle --release
-cargo bundle --release --target=x86_64-apple-darwin
+cargo bundle --release --target=$BUILD_TARGET
 # Copy libusb to the app bundle
-LIBUSB_DYLIB_PATH=`otool -L target/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/x1-mk1-usb2midi | grep libusb | awk -F ' ' '{print $1}'`
+LIBUSB_DYLIB_PATH=`otool -L target/$BUILD_TARGET/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/x1-mk1-usb2midi | grep libusb | awk -F ' ' '{print $1}'`
 LIBUSB_DYLIB_NAME=`basename $LIBUSB_DYLIB_PATH`
 echo $LIBUSB_DYLIB_PATH
 echo $LIBUSB_DYLIB_NAME
-cp $LIBUSB_DYLIB_PATH target/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/
-cp $LIBUSB_DYLIB_PATH target/x86_64-apple-darwin/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/
+cp $LIBUSB_DYLIB_PATH target/$BUILD_TARGET/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/
 # Update the libusb path in the app bundle
-install_name_tool -change $LIBUSB_DYLIB_PATH @executable_path/$LIBUSB_DYLIB_NAME target/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/x1-mk1-usb2midi
-install_name_tool -change $LIBUSB_DYLIB_PATH @executable_path/$LIBUSB_DYLIB_NAME target/x86_64-apple-darwin/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/x1-mk1-usb2midi
+install_name_tool -change $LIBUSB_DYLIB_PATH @executable_path/$LIBUSB_DYLIB_NAME target/$BUILD_TARGET/release/bundle/osx/X1Mk1\ usb2midi.app/Contents/MacOS/x1-mk1-usb2midi
 # Zip the app bundle
-cd ./target/release/bundle/osx ; zip X1Mk1-usb2midi_aarch64.zip * -r ; cd -
-cd ./target/x86_64-apple-darwin/release/bundle/osx ; zip X1Mk1-usb2midi_x86_64.zip * -r ; cd -
+cd ./target/$BUILD_TARGET/release/bundle/osx ; zip X1Mk1-usb2midi_$BUILD_TARGET.zip * -r ; cd -
