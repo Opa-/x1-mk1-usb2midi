7c2b222b95ba5fd716c9822540f8dde62301bfd2
Vincent Letarouilly <vincent.letarouilly@protonmail.com>
2026-02-04 13:29:32 +0100

    fix(build) self-signing macos app

--- bundle.sh
+++ bundle.sh
@@ -20,7 +20,11 @@ cp $LIBUSB_DYLIB_PATH "$APP_BUNDLE_PATH/Contents/MacOS/"
 
 install_name_tool -change $LIBUSB_DYLIB_PATH @executable_path/$LIBUSB_DYLIB_NAME "$APP_BUNDLE_PATH/Contents/MacOS/x1-mk1-usb2midi"
 
+echo "Self-signing app"
+codesign --force --deep --sign - "$APP_BUNDLE_PATH"
+
 cd ./target/$BUILD_TARGET/release/bundle/osx
+
 
 ZIP_NAME="X1Mk1-usb2midi_${BUILD_TARGET}_${OS_VERSION}.zip"
 zip "$ZIP_NAME" * -r
